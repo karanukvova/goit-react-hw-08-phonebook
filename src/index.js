4b63423cf771b4687862210c28aebcc1a440c7fd
karanukvova <79756306+karanukvova@users.noreply.github.com>
2023-08-13 19:31:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={<Loader />} persistor={persistor}>
       <BrowserRouter
-      basename='/goit-react-hw-08'
+      basename='/goit-react-hw-08-phonebook'
       >
         <App />
         <ToastContainer
